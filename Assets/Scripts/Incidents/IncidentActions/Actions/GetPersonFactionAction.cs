b4c925daa321704e8cb7da9fbeafc0e1494e628b
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-04 14:52:53 -0500

    Create GetPersonFactionAction to more easily retrive a person's faction

---
+++ Assets/Scripts/Incidents/IncidentActions/Actions/GetPersonFactionAction.cs
@@ -0,0 +1,13 @@
+﻿namespace Game.Incidents
+{
+	public class GetPersonFactionAction : GenericIncidentAction
+	{
+		public ContextualIncidentActionField<Person> person;
+		public ActionResultField<Faction> faction;
+
+		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
+		{
+			faction.SetValue(person.GetTypedFieldValue().Faction);
+		}
+	}
+}
