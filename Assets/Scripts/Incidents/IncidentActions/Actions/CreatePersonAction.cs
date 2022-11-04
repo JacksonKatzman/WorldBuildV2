ca208f71f5844e8de05a6ef12ffed2d01d457eb6
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-04 14:53:27 -0500

    Add CreatePersonAction which lays out params for creating a person, and creates it and adds to World contexts dictionary

---
+++ Assets/Scripts/Incidents/IncidentActions/Actions/CreatePersonAction.cs
@@ -0,0 +1,38 @@
+﻿using Game.Enums;
+using Game.Simulation;
+using System;
+using System.Collections.Generic;
+
+namespace Game.Incidents
+{
+	public class CreatePersonAction : GenericIncidentAction
+	{
+		public ContextualIncidentActionField<Person> parent;
+		public ContextualIncidentActionField<Race> race;
+		public ContextualIncidentActionField<Faction> faction;
+		public Gender gender;
+		public IntegerRange age;
+		public IntegerRange politicalPriority;
+		public IntegerRange economicPriority;
+		public IntegerRange religiousPriority;
+		public IntegerRange militaryPriority;
+		public IntegerRange influence;
+		public IntegerRange wealth;
+		public IntegerRange strength;
+		public IntegerRange dexterity;
+		public IntegerRange constitution;
+		public IntegerRange intelligence;
+		public IntegerRange wisdom;
+		public IntegerRange charisma;
+		public ActionResultField<Person> personResult;
+
+		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
+		{
+			var parents = parent.GetTypedFieldValue() != null ? new List<Person>() { parent.GetTypedFieldValue() } : null;
+			var person = new Person(age, gender, race.GetTypedFieldValue(), faction.GetTypedFieldValue(), politicalPriority,
+				economicPriority, religiousPriority, militaryPriority, influence, wealth, strength, dexterity, constitution,
+				intelligence, wisdom, charisma, null, parents);
+			SimulationManager.Instance.world.AddContext(person);
+		}
+	}
+}
