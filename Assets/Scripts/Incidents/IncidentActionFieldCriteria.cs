d5b9f78abb1b7fea67e9e60f2b42cffd6ef93e62
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-05 15:59:03 -0500

    Make specific type for criteria when searched for by an action

---
+++ Assets/Scripts/Incidents/IncidentActionFieldCriteria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Game.Incidents
+{
+	public class IncidentActionFieldCriteria : IncidentCriteria
+	{
+        public IncidentActionFieldCriteria() { }
+        public IncidentActionFieldCriteria(Type contextType) : base(contextType) { }
+
+		protected override bool IsValidPropertyType(Type type)
+		{
+            return base.IsValidPropertyType(type) || type == typeof(Faction);
+        }
+
+		protected override void SetPrimitiveType()
+		{
+			base.SetPrimitiveType();
+
+			if (PrimitiveType == typeof(Faction))
+			{
+				evaluator = new FactionEvaluator();
+			}
+		}
+	}
+}
