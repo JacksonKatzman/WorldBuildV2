bfb9511856c92688e525ca53d1c1abb7a6b22887
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-06 14:08:54 -0600

    Update City context

--- Assets/Scripts/Incidents/Contexts/City.cs
+++ Assets/Scripts/Incidents/Contexts/City.cs
@@ -1,10 +1,24 @@
-﻿using Game.Incidents;
+﻿using Game.Generators.Items;
+using Game.Incidents;
 using System;
+using System.Collections.Generic;
 
 namespace Game.Incidents
 {
-	public class City
+	public class City : InertIncidentContext
 	{
+		public override Type ContextType => typeof(City);
+		public Faction Faction { get; set; }
+		public int Population { get; set; }
+		public int Wealth { get; set; }
+		public List<Resource> Resources { get; set; }
+		public List<Landmark> Landmarks { get; set; }
 
+		public City(Faction faction, int population, int wealth)
+		{
+			Faction = faction;
+			Population = population;
+			Wealth = wealth;
+		}
 	}
 }
