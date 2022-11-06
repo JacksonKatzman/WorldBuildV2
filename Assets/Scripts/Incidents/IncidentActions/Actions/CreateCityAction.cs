5f20e2130167531ea3e45cc59bdacb1d2e43ceb2
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-06 14:10:03 -0600

    Create and Destroy City actions

---
+++ Assets/Scripts/Incidents/IncidentActions/Actions/CreateCityAction.cs
@@ -0,0 +1,20 @@
+﻿using Game.Simulation;
+using System;
+
+namespace Game.Incidents
+{
+	public class CreateCityAction : GenericIncidentAction
+	{
+		ContextualIncidentActionField<Faction> faction;
+		public IntegerRange population;
+		public IntegerRange wealth;
+		public ActionResultField<City> resultCity;
+		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
+		{
+			var city = new City(faction.GetTypedFieldValue(), population, wealth);
+			SimulationManager.Instance.world.AddContext(city);
+			resultCity.SetValue(city);
+			OutputLogger.Log("City created!");
+		}
+	}
+}
---
+++ Assets/Scripts/Incidents/IncidentActions/Actions/DestroyCityAction.cs
@@ -0,0 +1,15 @@
+﻿using Game.Simulation;
+
+namespace Game.Incidents
+{
+	public class DestroyCityAction : GenericIncidentAction
+	{
+		ContextualIncidentActionField<City> city;
+
+		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
+		{
+			SimulationManager.Instance.world.RemoveContext(city.GetTypedFieldValue());
+			OutputLogger.Log("City destroyed!");
+		}
+	}
+}
