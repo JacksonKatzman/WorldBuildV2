7285eb4fff89a8c5f72a4c48174394981412ad6a
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-07 17:33:15 -0600

    Added/Updated how to search for cells

--- Assets/Scripts/Simulation/SimulationUtilities.cs
+++ Assets/Scripts/Simulation/SimulationUtilities.cs
@@ -2,6 +2,7 @@
 using Game.Incidents;
 using Game.Terrain;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Game.Simulation
 {
@@ -14,22 +15,53 @@ namespace Game.Simulation
 
 		public static bool GetRandomUnclaimedCellIndex(out int index)
 		{
-			var found = false;
-			var cellIndex = 0;
-			index = -1;
-
-			for (int i = 0; i < 250 && !found; i++)
+			var grid = SimulationManager.Instance.HexGrid;
+			var claimedList = new List<int>();
+			foreach (var context in SimulationManager.Instance.world.Contexts[typeof(Faction)])
 			{
-				cellIndex = GetRandomCellIndex();
-				found = IsCellIndexUnclaimed(cellIndex);
+				claimedList.AddRange(((Faction)context).ControlledTileIndices);
 			}
-
-			if(found)
+			var unclaimed = grid.cells.Where(x => !claimedList.Contains(x.Index)).ToList();
+			if (unclaimed.Count > 0)
 			{
-				index = cellIndex;
+				index = unclaimed[SimRandom.RandomRange(0, unclaimed.Count)].Index;
+				return true;
 			}
+			else
+			{
+				index = 0;
+				return false;
+			}
+		}
 
-			return found;
+		public static bool GetRandomEmptyCellIndex(out int index)
+		{
+			var grid = SimulationManager.Instance.HexGrid;
+			var claimedList = GetClaimedCells();
+
+			var unclaimed = grid.cells.Where(x => !claimedList.Contains(x.Index)).ToList();
+			if (unclaimed.Count > 0)
+			{
+				index = unclaimed[SimRandom.RandomRange(0, unclaimed.Count)].Index;
+				return true;
+			}
+			else
+			{
+				index = 0;
+				return false;
+			}
+		}
+
+		public static List<int> GetEmptyTilesFromList(List<int> tiles)
+		{
+			var claimedList = GetClaimedCells();
+			return tiles.Where(x => !claimedList.Contains(x)).ToList();
+		}
+
+		public static List<int> GetCitylessTilesFromList(List<int> tiles)
+		{
+			var claimedList = GetCellsWithCities();
+			return tiles.Where(x => !claimedList.Contains(x)).ToList();
 		}
 
 		public static bool IsCellIndexUnclaimed(int index)
@@ -51,6 +83,34 @@ namespace Game.Simulation
 			var grid = SimulationManager.Instance.HexGrid;
 			var numCells = grid.cellCountX * grid.cellCountZ;
 			return grid.GetCell(SimRandom.RandomRange(0, numCells));
+		}
+
+		public static List<int> GetClaimedCells()
+		{
+			var claimedList = new List<int>();
+			foreach (var pair in SimulationManager.Instance.world.Contexts)
+			{
+				if (pair.Key.IsAssignableFrom(typeof(ILocationAffiliated)))
+				{
+					foreach (var context in pair.Value)
+					{
+						claimedList.Add(((ILocationAffiliated)context).CurrentLocation.TileIndex);
+					}
+				}
+			}
+
+			return claimedList;
+		}
+
+		public static List<int> GetCellsWithCities()
+		{
+			var claimedList = new List<int>();
+			foreach (var city in SimulationManager.Instance.world.Contexts[typeof(City)])
+			{
+				claimedList.Add(((City)city).CurrentLocation.TileIndex);
+			}
+
+			return claimedList;
 		}
 	}
 }
