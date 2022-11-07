e7d68f84659149edf94883854918f636416abac0
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-07 17:33:34 -0600

    Updated CreateCityAction to use new LocationActionField

--- Assets/Scripts/Incidents/IncidentActions/Actions/CreateCityAction.cs
+++ Assets/Scripts/Incidents/IncidentActions/Actions/CreateCityAction.cs
@@ -5,13 +5,14 @@ namespace Game.Incidents
 {
 	public class CreateCityAction : GenericIncidentAction
 	{
-		ContextualIncidentActionField<Faction> faction;
+		public ContextualIncidentActionField<Faction> faction;
+		public LocationActionField location;
 		public IntegerRange population;
 		public IntegerRange wealth;
 		public ActionResultField<City> resultCity;
 		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
 		{
-			var city = new City(faction.GetTypedFieldValue(), population, wealth);
+			var city = new City(faction.GetTypedFieldValue(), location.GetTypedFieldValue(), population, wealth);
 			SimulationManager.Instance.world.AddContext(city);
 			resultCity.SetValue(city);
 			OutputLogger.Log("City created!");
