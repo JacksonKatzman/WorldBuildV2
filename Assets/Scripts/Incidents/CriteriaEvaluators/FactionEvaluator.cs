dd5e13990df1bd268c5f4a80beb16faf3cbbb2c5
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-08 00:26:41 -0600

    Remove comments

--- Assets/Scripts/Incidents/CriteriaEvaluators/FactionEvaluator.cs
+++ Assets/Scripts/Incidents/CriteriaEvaluators/FactionEvaluator.cs
@@ -4,21 +4,6 @@
 	{
 		protected override Faction GetContext(IIncidentContext context)
 		{
-			/*
-			if (context.ContextType == typeof(Faction))
-			{
-				return (Faction)context;
-			}
-			else if (context.ContextType == typeof(Person))
-			{
-				return ((Person)context).AffiliatedFaction;
-			}
-			else
-			{
-				return null;
-			}
-			*/
-
 			if (context.ContextType.IsAssignableFrom(typeof(IFactionAffiliated)))
 			{
 				return ((IFactionAffiliated)context).AffiliatedFaction;
