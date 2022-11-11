879d60808afe6d1bc73d7da0c6a7ed4e21ed789d
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-11 12:32:35 -0600

    Change Control of Territory Action

---
+++ Assets/Scripts/Incidents/IncidentActions/Actions/ChangeControlOfTerritoryAction.cs
@@ -0,0 +1,27 @@
+﻿namespace Game.Incidents
+{
+	public class ChangeControlOfTerritoryAction : GenericIncidentAction
+	{
+		public ContextualIncidentActionField<Faction> territoryGainer;
+		public ContextualIncidentActionField<Faction> territoryLoser;
+		public LocationActionField location;
+
+		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
+		{
+			var gainer = territoryGainer.GetTypedFieldValue();
+			var loser = territoryLoser.GetTypedFieldValue();
+			var tileIndex = location.GetTypedFieldValue().TileIndex;
+
+			if (!gainer.ControlledTileIndices.Contains(tileIndex))
+			{
+				gainer.ControlledTileIndices.Add(tileIndex);
+			}
+			if(loser.ControlledTileIndices.Contains(tileIndex))
+			{
+				loser.ControlledTileIndices.Remove(tileIndex);
+			}
+
+			//Might need to notify tile inhabitants later so they can adjust for the change
+		}
+	}
+}
