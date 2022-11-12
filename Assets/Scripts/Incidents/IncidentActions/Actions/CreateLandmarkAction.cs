4927ad1576cec627d9b2389cad79e73b4b4aebb8
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-12 12:41:24 -0600

    Create/Destroy Landmark actions

---
+++ Assets/Scripts/Incidents/IncidentActions/Actions/CreateLandmarkAction.cs
@@ -0,0 +1,17 @@
+﻿using Game.Simulation;
+using System;
+
+namespace Game.Incidents
+{
+	public class CreateLandmarkAction : GenericIncidentAction
+	{
+		public LocationActionField location;
+		public ActionResultField<Landmark> resultLandmark;
+		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
+		{
+			var landmark = new Landmark(location.GetTypedFieldValue());
+			SimulationManager.Instance.world.AddContext(landmark);
+			resultLandmark.SetValue(landmark);
+		}
+	}
+}
