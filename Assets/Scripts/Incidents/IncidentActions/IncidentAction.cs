32a645b0239e183f20cab5fa0b935f2975c00669
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-11 18:26:45 -0600

    Change IncidentAction to handle updating lists of context fields as well

--- Assets/Scripts/Incidents/IncidentActions/IncidentAction.cs
+++ Assets/Scripts/Incidents/IncidentActions/IncidentAction.cs
@@ -11,6 +11,7 @@ namespace Game.Incidents
 		virtual public bool VerifyAction(IIncidentContext context, Func<int, IIncidentActionField> delayedCalculateAction)
 		{
 			var matchingFields = GetContexualActionFields();
+			var matchingLists = GetCollectionsOfActionFields();
 
 			foreach (var field in matchingFields)
 			{
@@ -18,6 +19,18 @@ namespace Game.Incidents
 				if (!actionField.CalculateField(context, delayedCalculateAction))
 				{
 					return false;
+				}
+			}
+
+			foreach (var l in matchingLists)
+			{
+				var list = l.GetValue(this) as List<IncidentActionFieldContainer>;
+				foreach (var actionFieldContainer in list)
+				{
+					if (!actionFieldContainer.actionField.CalculateField(context, delayedCalculateAction))
+					{
+						return false;
+					}
 				}
 			}
 
@@ -35,6 +48,13 @@ namespace Game.Incidents
 				field.SetValue(this, Activator.CreateInstance(field.FieldType, IncidentEditorWindow.ContextType));
 			}
 
+			matchingFields = GetCollectionsOfActionFields();
+
+			foreach(var field in matchingFields)
+			{
+				field.SetValue(this, Activator.CreateInstance(field.FieldType));
+			}
+
 			matchingFields = GetIntegerRangeFields();
 
 			foreach (var field in matchingFields)
@@ -46,6 +66,7 @@ namespace Game.Incidents
 		virtual public void UpdateActionFieldIDs(ref int startingValue)
 		{
 			var matchingFields = GetContexualActionFields();
+			var matchingLists = GetCollectionsOfActionFields();
 
 			foreach (var f in matchingFields)
 			{
@@ -55,22 +76,46 @@ namespace Game.Incidents
 				IncidentEditorWindow.actionFields.Add(fa);
 				startingValue++;
 			}
+
+			foreach(var l in matchingLists)
+			{
+				var list = l.GetValue(this) as List<IncidentActionFieldContainer>;
+				foreach(var f in list)
+				{
+					f.actionField.ActionFieldID = startingValue;
+					f.actionField.NameID = string.Format("{0}:{1}:{2}", f.actionField.ActionFieldIDString, GetType().Name, l.Name);
+					IncidentEditorWindow.actionFields.Add(f.actionField);
+					startingValue++;
+				}
+			}
 		}
 
 		virtual public void AddContext(ref IncidentReport report)
 		{
 			var matchingFields = GetContexualActionFields();
+			var matchingLists = GetCollectionsOfActionFields();
 
 			foreach (var field in matchingFields)
 			{
 				var actionField = field.GetValue(this) as IIncidentActionField;
 				report.Contexts.Add(actionField.ActionFieldIDString, actionField.GetFieldValue());
 			}
+
+			foreach (var l in matchingLists)
+			{
+				var list = l.GetValue(this) as List<IncidentActionFieldContainer>;
+				foreach (var actionFieldContainer in list)
+				{
+					var actionField = actionFieldContainer.actionField;
+					report.Contexts.Add(actionField.ActionFieldIDString, actionField.GetFieldValue());
+				}
+			}
 		}
 
 		virtual public bool GetContextField(int id, out IIncidentActionField contextField)
 		{
 			var matchingFields = GetContexualActionFields();
+			var matchingLists = GetCollectionsOfActionFields();
 
 			foreach (var field in matchingFields)
 			{
@@ -79,6 +124,20 @@ namespace Game.Incidents
 				{
 					contextField = actionField;
 					return true;
+				}
+			}
+
+			foreach (var l in matchingLists)
+			{
+				var list = l.GetValue(this) as List<IncidentActionFieldContainer>;
+				foreach (var actionFieldContainer in list)
+				{
+					var actionField = actionFieldContainer.actionField;
+					if (actionField.ActionFieldID == id)
+					{
+						contextField = actionField;
+						return true;
+					}
 				}
 			}
 
@@ -91,6 +150,13 @@ namespace Game.Incidents
 			var fields = this.GetType().GetFields();
 			return fields.Where(x => (x.FieldType.IsGenericType && (x.FieldType.GetGenericTypeDefinition() == typeof(ContextualIncidentActionField<>)
 			|| x.FieldType.GetGenericTypeDefinition() == typeof(ActionResultField<>))) || x.FieldType == typeof(LocationActionField));
+		}
+
+		private IEnumerable<FieldInfo> GetCollectionsOfActionFields()
+		{
+			var fields = this.GetType().GetFields();
+			var lists = fields.Where(x => x.FieldType.IsGenericType && x.FieldType.GetGenericTypeDefinition() == typeof(List<>) && x.FieldType.GetGenericArguments()[0] == typeof(IncidentActionFieldContainer));
+			return lists;
 		}
 
 		private IEnumerable<FieldInfo> GetIntegerRangeFields()
