602bccd49fac11bc1ff8bd1d0948c9c7f5970928
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-14 16:55:27 -0600

    Testing expressions with ranges stub - this will need to be worked on next

--- Assets/Scripts/Data/IntegerRange.cs
+++ Assets/Scripts/Data/IntegerRange.cs
@@ -1,9 +1,10 @@
 ﻿using Sirenix.OdinInspector;
+using System;
 
 namespace Game.Incidents
 {
 	[HideReferenceObjectPicker, InlineProperty]
-	public class IntegerRange
+	public class IntegerRange : ValueRange<int>
 	{
 		[HorizontalGroup("A", LabelWidth = 52, Width = 40)]
 		public bool randomRange = true;
@@ -14,7 +15,7 @@ namespace Game.Incidents
 		public int constant;
 
 		public static implicit operator int(IntegerRange i) => i.Value;
-		public int Value
+		override public int Value
 		{
 			get
 			{
@@ -24,6 +25,56 @@ namespace Game.Incidents
 		private int Calculate()
 		{
 			return SimRandom.RandomRange(min, max);
+		}
+	}
+
+	public class FloatRange : ValueRange<float>
+	{
+		public override float Value => 0.0f;
+	}
+
+	public class BoolRange : ValueRange<bool>
+	{
+		public override bool Value => false;
+	}
+
+	public interface IValueRange 
+	{
+		Type GetValueType { get; }
+	}
+
+	public class ValueRange<T> : IValueRange
+	{
+		virtual public T Value => default(T);
+		public Type GetValueType => typeof(T);
+	}
+
+	public static class ValueRangeFactory
+	{
+		public static IValueRange CreateValueRange<T>()
+		{
+			var type = typeof(T);
+			if(type == typeof(int))
+			{
+				return new IntegerRange();
+			}
+			else if(type == typeof(float))
+			{
+				return new FloatRange();
+			}
+			else if(type == typeof(bool))
+			{
+				return new BoolRange();
+			}
+			else
+			{
+				return new ValueRange<T>();
+			}
+		}
+
+		public static T FetchValue<T>(IValueRange range)
+		{
+			return ((ValueRange<T>)range).Value;
 		}
 	}
 }
