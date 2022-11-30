18331f9c17bd76beada87259fe53c271e687508e
Jackson Katzman <jackson@jacksonkatzman.com>
2022-11-30 15:16:38 -0600

    Fixed relation assumption if war state action and added change control of city action

---
+++ Assets/Scripts/Incidents/IncidentActions/Actions/ChangeControlOfCityAction.cs
@@ -0,0 +1,37 @@
+﻿namespace Game.Incidents
+{
+	public class ChangeControlOfCityAction : GenericIncidentAction
+	{
+		public ContextualIncidentActionField<Faction> cityGainer;
+		public ContextualIncidentActionField<Faction> cityLoser;
+		public ContextualIncidentActionField<City> city;
+
+		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
+		{
+			var gainer = cityGainer.GetTypedFieldValue();
+			var loser = cityLoser.GetTypedFieldValue();
+			var c = city.GetTypedFieldValue();
+			var tileIndex = c.CurrentLocation.TileIndex;
+
+			if(!gainer.Cities.Contains(c))
+			{
+				gainer.Cities.Add(c);
+			}
+			if(loser.Cities.Contains(c))
+			{
+				loser.Cities.Remove(c);
+			}
+
+			if (!gainer.ControlledTileIndices.Contains(tileIndex))
+			{
+				gainer.ControlledTileIndices.Add(tileIndex);
+			}
+			if (loser.ControlledTileIndices.Contains(tileIndex))
+			{
+				loser.ControlledTileIndices.Remove(tileIndex);
+			}
+
+			//Might need to notify tile inhabitants later so they can adjust for the change
+		}
+	}
+}
