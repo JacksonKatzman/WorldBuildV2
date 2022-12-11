b4eeb65da2480d772513b242aa35d73bb0ae983f
Jackson Katzman <jackson@jacksonkatzman.com>
2022-12-11 14:54:32 -0600

    Created separate actions for trading one item vs all

--- Assets/Scripts/Incidents/IncidentActions/Actions/TradeAllItemsAction.cs
+++ Assets/Scripts/Incidents/IncidentActions/Actions/TradeAllItemsAction.cs
@@ -1,22 +1,15 @@
-﻿using Game.Generators.Items;
-
-namespace Game.Incidents
+﻿namespace Game.Incidents
 {
-	public class TradeItemsAction : GenericIncidentAction
+	public class TradeAllItemsAction : GenericIncidentAction
 	{
-		public ContextualIncidentActionField<Item> itemField;
 		public InterfacedIncidentActionFieldContainer<IInventoryAffiliated> givingInventory;
 		public InterfacedIncidentActionFieldContainer<IInventoryAffiliated> receivingInventory;
 		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
 		{
 			var giver = ((IInventoryAffiliated)givingInventory.actionField.GetFieldValue());
 			var receiver = ((IInventoryAffiliated)receivingInventory.actionField.GetFieldValue());
-			var item = itemField.GetTypedFieldValue();
-			if(giver.Inventory.Items.Contains(item))
-			{
-				giver.Inventory.Items.Remove(item);
-				receiver.Inventory.Items.Add(item);
-			}
+			receiver.Inventory.Items.AddRange(giver.Inventory.Items);
+			giver.Inventory.Items.Clear();
 		}
 	}
 }
