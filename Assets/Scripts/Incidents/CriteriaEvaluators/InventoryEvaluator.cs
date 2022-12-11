cb190951c5fa677cc912a4c77ed5c3489d884893
Jackson Katzman <jackson@jacksonkatzman.com>
2022-12-11 14:54:11 -0600

    Made Inventory it's own context so it could be used in an InventoryEvaluator

---
+++ Assets/Scripts/Incidents/CriteriaEvaluators/InventoryEvaluator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Game.Incidents
+{
+	public class InventoryEvaluator : ContextEvaluator<Inventory, IInventoryAffiliated>
+	{
+		public InventoryEvaluator() : base() { }
+		public InventoryEvaluator(string propertyName, Type contextType) : base(propertyName, contextType) { }
+
+		protected override Inventory GetContext(IIncidentContext context)
+		{
+			if ((typeof(IInventoryAffiliated)).IsAssignableFrom(context.ContextType))
+			{
+				return ((IInventoryAffiliated)context).Inventory;
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
