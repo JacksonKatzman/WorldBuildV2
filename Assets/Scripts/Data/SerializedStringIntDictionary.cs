40125c45183c452a9e6a89649f9450dac1183eed
Jackson Katzman <jackson@jacksonkatzman.com>
2023-01-02 12:59:33 -0600

    Serializable Dictionaries

---
+++ Assets/Scripts/Data/SerializedStringIntDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[Serializable]
+public class SerializedStringIntDictionary : UnitySerializedDictionary<string, int>
+{
+	public static SerializedStringIntDictionary Merge(SerializedStringIntDictionary a, SerializedStringIntDictionary b)
+	{
+		var result = new SerializedStringIntDictionary();
+
+		result = (SerializedStringIntDictionary)a.Union(b).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+		return result;
+	}
+}
