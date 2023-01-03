7cd187ae2ae2a86e47eec48545eee04fbc2d8f67
Jackson Katzman <jackson@jacksonkatzman.com>
2023-01-02 18:29:16 -0600

    More groundwork for naming

---
+++ Assets/Scripts/Generators/Names/NamingThemeCollectionContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Generators.Names
+{
+	[Serializable]
+	[CreateAssetMenu(fileName = "ThemeCollection", menuName = "ScriptableObjects/Names/Theme Collection", order = 2)]
+	public class NamingThemeCollectionContainer : ScriptableObject
+	{
+		public NamingThemeCollection collection;
+
+		private void Awake()
+		{
+			collection = new NamingThemeCollection();
+		}
+	}
+}
---
+++ Assets/Scripts/Generators/Names/NamingThemePreset.cs
@@ -0,0 +1,17 @@
+﻿using Game.Data;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Generators.Names
+{
+	[CreateAssetMenu(fileName = "NamingTheme", menuName = "ScriptableObjects/Names/Theme Preset", order = 1)]
+	public class NamingThemePreset : ScriptableObject
+	{
+		[SerializeField]
+		public List<NamingThemeCollectionContainer> themeCollections;
+
+		public int averageFirstNameSyllables;
+		public int averageSurnameSyllables;
+		public WeightedListSerializableDictionary<string> surnameFormats;
+	}
+}
