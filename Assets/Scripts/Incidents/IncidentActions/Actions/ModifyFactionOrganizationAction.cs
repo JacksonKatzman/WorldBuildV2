7d07c9c865ed6fce011dd3c4fedbf3b95dadcd37
Jackson Katzman <jackson@jacksonkatzman.com>
2023-01-20 15:56:59 -0600

    Temporary simple way for organizations to upgrade during the sim

---
+++ Assets/Scripts/Incidents/IncidentActions/Actions/ModifyFactionOrganizationAction.cs
@@ -0,0 +1,12 @@
+﻿namespace Game.Incidents
+{
+	public class ModifyFactionOrganizationAction : GenericIncidentAction
+	{
+		public ContextualIncidentActionField<Faction> faction;
+		public Enums.OrganizationType organizationType;
+		public override void PerformAction(IIncidentContext context, ref IncidentReport report)
+		{
+			faction.GetTypedFieldValue().Government.UpdateHierarchy();
+		}
+	}
+}
