245c1bac493c04821f8c4ab2ef1239b7dbb3e4a4
Jackson Katzman <jackson@jacksonkatzman.com>
2023-01-24 19:37:34 -0600

    Move adventures code to its own directory

---
+++ Assets/Scripts/Simulation/Adventures/AdventureService.cs
@@ -0,0 +1,42 @@
+﻿using Game.IO;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Game.Simulation
+{
+	public class AdventureService
+	{
+		private static AdventureService instance;
+		public static AdventureService Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new AdventureService();
+				}
+				return instance;
+			}
+		}
+
+		public List<AdventureEncounter> encounters;
+		public List<AdventureEncounter> usedEncounters;
+
+		public AdventureService()
+		{
+			Setup();
+		}
+
+		private void Setup()
+		{
+			encounters = new List<AdventureEncounter>();
+			usedEncounters = new List<AdventureEncounter>();
+
+			var path = "ScriptableObjects/Encounters";
+
+			encounters.AddRange(Resources.LoadAll(path, typeof(AdventureEncounter)).Cast<AdventureEncounter>().ToList());
+			OutputLogger.Log(string.Format("{0} encounters loaded.", encounters.Count));
+		}
+	}
+}
