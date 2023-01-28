5cae717bc379360c5a3aea3893db6d7308c8c078
Jackson Katzman <jackson@jacksonkatzman.com>
2023-01-28 16:52:01 -0600

    Swap stuff over to a utilities namespace

---
+++ Assets/Scripts/Utilities/StringUtilities.cs
@@ -0,0 +1,19 @@
+﻿namespace Game.Utilities
+{
+	public static class StringUtilities
+	{
+		public static string ReplaceFirstOccurence(string source, string find, string replace)
+		{
+			int place = source.IndexOf(find);
+			string result = source.Remove(place, find.Length).Insert(place, replace);
+			return result.Replace("\r", "");
+		}
+
+		public static string ReplaceLastOccurrence(string source, string find, string replace)
+		{
+			int place = source.LastIndexOf(find);
+			string result = source.Remove(place, find.Length).Insert(place, replace);
+			return result;
+		}
+	}
+}
