29b2d710b4c359e80493ebcdb7de75221b808691
Jackson Katzman <jackson@jacksonkatzman.com>
2023-02-24 15:09:12 -0600

    Adding tooltips

---
+++ Assets/Scripts/GUI/Adventures/GenericTooltipTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game.GUI.Wiki
+{
+	public class GenericTooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+	{
+		private static LTDescr delay;
+		public string header;
+		[TextArea(15, 20)]
+		public string content;
+		public void OnPointerEnter(PointerEventData eventData)
+		{
+			delay = LeanTween.delayedCall(0.75f, () =>
+			{
+				TooltipService.ShowTooltip(content, header);
+			});
+		}
+
+		public void OnPointerExit(PointerEventData eventData)
+		{
+			LeanTween.cancel(delay.uniqueId);
+			TooltipService.HideTooltip();
+		}
+	}
+}
---
+++ Assets/Scripts/GUI/Adventures/TooltipService.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Game.GUI.Wiki
+{
+	public class TooltipService : MonoBehaviour
+	{
+		public static TooltipService Instance { get; private set; }
+		public Tooltip tooltip;
+
+		private void Awake()
+		{
+			if(Instance != null && Instance != this)
+			{
+				Destroy(this);
+			}
+			else
+			{
+				Instance = this;
+			}
+		}
+
+		public static void ShowTooltip(string content, string header = "")
+		{
+			Instance.tooltip.header.enabled = !string.IsNullOrEmpty(header);
+			Instance.tooltip.header.text = header;
+			Instance.tooltip.content.text = content;
+
+			Instance.tooltip.gameObject.SetActive(true);
+		}
+
+		public static void HideTooltip()
+		{
+			Instance.tooltip.gameObject.SetActive(false);
+		}
+	}
+}
