1ca32e96f13e5f0310fce0e5cf60808e3f10abe7
Jackson Katzman <jackson@jacksonkatzman.com>
2023-02-28 11:09:05 -0600

    Add ConstantListAttributes for future use

---
+++ Assets/Scripts/Data/ConstantListAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Utilities
+{
+	[AttributeUsage(AttributeTargets.Field)]
+	public class ConstantListAttribute : PropertyAttribute
+	{
+		public IEnumerable<Type> Types { get; private set; }
+		public bool Inherited { get; set; }
+		public ConstantListAttribute(params Type[] types)
+		{
+			this.Types = types;
+		}
+	}
+}
---
+++ Assets/Scripts/Data/StringConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Game.Utilities
+{
+	public class PopupType
+	{
+		public const int CREATURE_INFO_CARD = 200;
+	}
+}
