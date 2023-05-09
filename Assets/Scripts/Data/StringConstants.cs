94540147e6f6dbf6fb1835309b5d0ac9edef2d11
Jackson Katzman <jackson@jacksonkatzman.com>
2023-05-09 10:16:23 -0500

    Create working PopupDefinitions to pair type ints to prefabs

--- Assets/Scripts/Data/StringConstants.cs
+++ Assets/Scripts/Data/StringConstants.cs
@@ -1,7 +1,9 @@
 ﻿namespace Game.Utilities
 {
-	public class PopupType
+	public class PopupType : IPopupTypeConstantSource
 	{
 		public const int CREATURE_INFO_CARD = 200;
 	}
+
+	public interface IPopupTypeConstantSource { }
 }
--- Assets/Scripts/GUI/Popups/PopupDefinition.cs
+++ Assets/Scripts/GUI/Popups/PopupDefinition.cs
@@ -1,14 +1,55 @@
 ﻿using Game.Utilities;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace Game.GUI.Popups
 {
 	[CreateAssetMenu(fileName = nameof(PopupDefinition), menuName = "ScriptableObjects/Popups/" + nameof(PopupDefinition), order = 1)]
-	public class PopupDefinition : ScriptableObject
+	public class PopupDefinition : SerializedScriptableObject
 	{
-		[SerializeField, ConstantList(typeof(PopupType))]
+		[ShowInInspector, ReadOnly]
 		private int popupType;
+		[SerializeField, ValueDropdown("ShowValues"), OnValueChanged("SetPopupType")]
+		private string popupTypeString;
 		[SerializeField]
 		private GameObject prefab;
+		private Dictionary<string, int> Constants => GetConstantsDictionary();
+
+			/*
+		private IEnumerable<string> ShowValues()
+		{
+			var fields = typeof(PopupType).GetFields(System.Reflection.BindingFlags.Public).Where(c => c.IsPublic && c.IsLiteral && !c.IsInitOnly && c.FieldType == typeof(int));
+			var options = new List<string>();
+			foreach(var field in fields)
+			{
+				var name = string.Format("{0} - {1}", field.Name, field.GetRawConstantValue());
+			}
+
+			return EditorGUILayout.Popup("Popup Type", value, options.ToArray());
+		}
+*/
+		private IEnumerable<string> ShowValues()
+		{
+			return Constants.Keys;
+		}
+
+		private void SetPopupType()
+		{
+			popupType = Constants[popupTypeString];
+		}
+
+		private Dictionary<string, int> GetConstantsDictionary()
+		{
+			var fields = typeof(PopupType).GetFields().Where(c => c.FieldType == typeof(int));
+			var dict = new Dictionary<string, int>();
+			foreach(var field in fields)
+			{
+				dict.Add(field.Name, (int)field.GetRawConstantValue());
+			}
+			return dict;
+		}
 	}
 }
