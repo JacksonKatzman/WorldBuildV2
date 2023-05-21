4d868716dc7dac39b9da5e1f2b76cf67fbce7af5
Jackson Katzman <jackson@jacksonkatzman.com>
2023-05-21 14:06:30 -0500

    Create HexCellLabel to use instead of calling messy GetComponents everywhere

---
+++ Assets/Scripts/Visuals/World/HexMap/Scripts/HexCellLabel.cs
@@ -0,0 +1,35 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Terrain
+{
+	public class HexCellLabel : SerializedMonoBehaviour
+	{
+		public RectTransform rectTransform;
+		public Text hexCellText;
+		public Image hexCellHighlight;
+		public Dictionary<HexDirection, Image> hexCellBorderImages;
+
+		public void SetText(string text)
+		{
+			hexCellText.text = text;
+		}
+
+		public void SetHighlightColor(Color color)
+		{
+			hexCellHighlight.color = color;
+		}
+
+		public void ToggleHighlight(bool isOn)
+		{
+			hexCellHighlight.enabled = isOn;
+		}
+
+		public void ToggleBorder(HexDirection direction, bool isOn)
+		{
+			hexCellBorderImages[direction].enabled = isOn;
+		}
+	}
+}
