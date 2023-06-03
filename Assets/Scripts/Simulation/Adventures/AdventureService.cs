08384c2b6e9d1be7c82fbe0efc28a96a61e56203
Jackson Katzman <jackson@jacksonkatzman.com>
2023-06-02 19:19:39 -0500

    Swapped to AssetCollection and began shell for beginning an adventure

--- Assets/Scripts/Simulation/Adventures/AdventureService.cs
+++ Assets/Scripts/Simulation/Adventures/AdventureService.cs
@@ -1,11 +1,13 @@
 ﻿using Game.Creatures;
+using Game.Incidents;
+using Game.Utilities;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 namespace Game.Simulation
 {
-	public class AdventureService
+	public class AdventureService : ILocationAffiliated
 	{
 		private static AdventureService instance;
 		public static AdventureService Instance
@@ -26,9 +28,54 @@ namespace Game.Simulation
 
 		public List<MonsterData> monsterData;
 
+		public Location CurrentLocation { get; set; }
+
 		public AdventureService()
 		{
 			Setup();
+		}
+
+		public void SetAdventureStartingPoint(Location location)
+		{
+			CurrentLocation = location;
+		}
+
+		public void BeginAdventure()
+		{
+			if(CurrentLocation == null)
+			{
+				OutputLogger.LogError("Current Location in AdventureService is null!");
+				return;
+			}
+
+			var range = 4; //more complex calc later based on party level etc
+			var cellsInRange = CurrentLocation.GetAllCellsInRange(range);
+			var encountersInRange = availableEncounters.Where(encounter => cellsInRange.Contains(encounter.CurrentLocation.GetHexCell()));
+
+			var adventureOfferingCount = 5; 
+			var lowerDifficultyThreshold = 0;
+			var upperDifficultyThreshold = 3;
+			var selectedEncounters = new List<AdventureEncounterObject>();
+
+			var levelAppropriateEncounters = GetLevelAppropriateEncounters(encountersInRange.ToList(), lowerDifficultyThreshold, upperDifficultyThreshold);
+			
+			while((selectedEncounters.Count < adventureOfferingCount) && (levelAppropriateEncounters.Count > 0))
+			{
+				var chosen = SimRandom.RandomEntryFromList(levelAppropriateEncounters);
+				selectedEncounters.Add(chosen);
+				levelAppropriateEncounters.Remove(chosen);
+			}
+
+			var levelAppropriateEvergreenEncounters = GetLevelAppropriateEncounters(evergreenEncounters, lowerDifficultyThreshold, upperDifficultyThreshold);
+			while(selectedEncounters.Count < adventureOfferingCount)
+			{
+				selectedEncounters.Add(SimRandom.RandomEntryFromList(levelAppropriateEvergreenEncounters));
+			}
+		}
+
+		public List<AdventureEncounterObject> GetLevelAppropriateEncounters(List<AdventureEncounterObject> possibleAdventures, int lowerDifficultyThreshold, int upperDifficultyThreshold)
+		{
+			return possibleAdventures.Where(encounter => encounter.encounterDifficulty >= lowerDifficultyThreshold && encounter.encounterDifficulty <= upperDifficultyThreshold).ToList();
 		}
 
 		public void AddAvailableEncounter(AdventureEncounterObject encounter)
