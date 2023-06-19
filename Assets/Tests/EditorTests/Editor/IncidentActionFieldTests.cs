d14ad84c6bcde39ba13f50cdaac90a09256c6792
Jackson Katzman <jackson@jacksonkatzman.com>
2023-06-19 13:23:31 -0500

    Create shells for two more sets of tests I want to later make

---
+++ Assets/Tests/EditorTests/Editor/IncidentActionFieldTests.cs
@@ -0,0 +1,32 @@
+﻿using Game.Incidents;
+using Game.Simulation;
+using NUnit.Framework;
+using UnityEditor;
+
+public class IncidentActionFieldTests : Editor
+{
+    [SetUp]
+    public void SetUp()
+    {
+        var current = new IncidentContextDictionary();
+        var all = new IncidentContextDictionary();
+        ContextDictionaryProvider.SetContextsProviders(() => current, () => all);
+        ContextDictionaryProvider.SetNextID(1);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        ContextDictionaryProvider.SetContextsProviders(() => null, () => null);
+        ContextDictionaryProvider.SetNextID(1);
+    }
+
+    [Test]
+    public void ContextualIncidentActionField_RetrieveValueFromPrevious()
+	{
+        var previousFaction = new Faction();
+        var previousFactionField = new ContextualIncidentActionField<Faction>() { value = previousFaction, ActionFieldID = 1 };
+
+        var factionField = new ContextualIncidentActionField<Faction>() { PreviousFieldID = 1 };
+	}
+}
