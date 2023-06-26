88ca7554f9e31d7a4507ed3503df1cc3b44eb416
Jackson Katzman <jackson@jacksonkatzman.com>
2023-06-26 12:56:35 -0500

    Temporarily make IncidentContextDictionary init with all required types to prevent collection from changing mid run

--- Assets/Scripts/Data/IncidentContextDictionary.cs
+++ Assets/Scripts/Data/IncidentContextDictionary.cs
@@ -1,10 +1,22 @@
-﻿using Game.Incidents;
+﻿using Game.Generators.Items;
+using Game.Incidents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Game.Simulation
 {
 	public class IncidentContextDictionary : TypeListDictionary<IIncidentContext>
 	{
+		public IncidentContextDictionary()
+		{
+			var types = GetRequiredTypes();
+			foreach(var type in types)
+			{
+				this.Add(type, new List<IIncidentContext>());
+			}
+		}
 		public IIncidentContext GetContextByID(int id)
 		{
 			foreach(var list in this.Values)
@@ -48,5 +60,30 @@ namespace Game.Simulation
 				}
 			}
 		}
+
+		private List<Type> GetRequiredTypes()
+		{
+			return new List<Type>()
+			{
+				typeof(Character), typeof(Faction),
+				typeof(Item), typeof(City),
+				typeof(Location), typeof(Landmark),
+				typeof(Race), typeof(Monster),
+				typeof(Resource), typeof(GreatMonster),
+				typeof(Organization)
+			};
+		}
+
+		/*
+		private IEnumerable<Type> GetAllMatchingContextTypes(Type type)
+		{
+			var q = type.Assembly.GetTypes()
+				.Where(x => !x.IsAbstract)                                          // Excludes BaseClass
+				.Where(x => !x.IsGenericTypeDefinition)                             // Excludes Generics
+				.Where(x => type.IsAssignableFrom(x));
+
+			return q;// Excludes classes not inheriting from IIncidentContext
+		}
+		*/
 	}
 }
