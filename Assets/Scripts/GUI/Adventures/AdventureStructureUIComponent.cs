76f954084b2b5d3648d50714c2c1f1e45e4c2936
Jackson Katzman <jackson@jacksonkatzman.com>
2023-07-10 22:46:52 -0500

    Add structure and combat ui prefabs

---
+++ Assets/Scripts/GUI/Adventures/AdventureStructureUIComponent.cs
@@ -0,0 +1,24 @@
+﻿using Game.Simulation;
+using System.Collections.Generic;
+using TMPro;
+
+namespace Game.GUI.Wiki
+{
+	public class AdventureStructureUIComponent : AdventureUIComponent<AdventureStructureComponent>
+	{
+		public TMP_Text structureNameText;
+		public TMP_Text structureDescription;
+		public TMP_Text ceilingDescription;
+		public TMP_Text floorsAndWallsDescription;
+		public TMP_Text doorsDescription;
+		public TMP_Text lightingDescription;
+
+		protected override List<TMP_Text> AssociatedTexts => new List<TMP_Text>() { structureNameText, structureDescription,
+			ceilingDescription, floorsAndWallsDescription, doorsDescription, lightingDescription };
+
+		public override void BuildUIComponents(AdventureStructureComponent component)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
