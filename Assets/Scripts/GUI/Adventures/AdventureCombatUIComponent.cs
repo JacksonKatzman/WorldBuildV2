835b59165bd3a6a9966e2eb065442ec8f3826992
Jackson Katzman <jackson@jacksonkatzman.com>
2023-07-10 22:45:25 -0500

    Finish implementing combat component

--- Assets/Scripts/GUI/Adventures/AdventureCombatUIComponent.cs
+++ Assets/Scripts/GUI/Adventures/AdventureCombatUIComponent.cs
@@ -1,29 +1,27 @@
 ﻿using Game.Simulation;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine.EventSystems;
 
 namespace Game.GUI.Wiki
 {
 	public class AdventureCombatUIComponent : AdventureUIComponent<AdventureCombatComponent>
 	{
+		public TMP_Text participantsText;
+		public TMP_Text descriptionText;
+
+		protected override List<TMP_Text> AssociatedTexts => new List<TMP_Text>() { participantsText, descriptionText };
+
 		public override void BuildUIComponents(AdventureCombatComponent component)
 		{
-			//throw new System.NotImplementedException();
-		}
+			descriptionText.text = component.description;
 
-		public override void OnPointerClick(PointerEventData eventData)
-		{
-			//throw new System.NotImplementedException();
-		}
-
-		public override void ReplaceTextPlaceholders(List<IAdventureContextCriteria> contexts)
-		{
-			//throw new System.NotImplementedException();
-		}
-
-		protected override void ToggleElements()
-		{
-			//throw new System.NotImplementedException();
+			foreach(var participant in component.combatParticipants)
+			{
+				var id = "{" + participant.combatantID + "}";
+				var entry = string.Format("<indent=1em>\u2022 {0} {1}. </indent> \n", participant.amount, id);
+				participantsText.text += entry;
+			}
 		}
 	}
 }
