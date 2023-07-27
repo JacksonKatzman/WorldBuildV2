9c62073f2a0a4e0fdccc46d03887e81feda1f684
Jackson Katzman <jackson@jacksonkatzman.com>
2023-07-27 12:05:28 -0500

    Create static fn for creating items based on ItemTypes

--- Assets/Scripts/Generators/Items/Item.cs
+++ Assets/Scripts/Generators/Items/Item.cs
@@ -11,6 +11,27 @@ namespace Game.Generators.Items
 		public override Type ContextType => typeof(Item);
 
 		public Inventory CurrentInventory { get; set; }
+		public ItemValue Value { get; set; }
+		public int Points { get; set; }
+
+		abstract public void RollStats(int points);
+
+		public static Item Create(ItemType itemType)
+		{
+			var type = itemType.GetType();
+			if(type == typeof(WeaponType))
+			{
+				return new Weapon(itemType as WeaponType);
+			}
+			else if(type == typeof(ArmorType))
+			{
+				return new Armor(itemType as ArmorType);
+			}
+			else
+			{
+				return new Trinket();
+			}
+		}
 	}
 
 	[System.Serializable]
