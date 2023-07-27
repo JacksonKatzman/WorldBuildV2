81f5b79f03f0f417a1425a2f10aa3b583dff7779
Jackson Katzman <jackson@jacksonkatzman.com>
2023-07-27 12:10:58 -0500

    Create AdventureTreasureUIComponent and prefab

---
+++ Assets/Scripts/GUI/Adventures/AdventureTreasureUIComponent.cs
@@ -0,0 +1,39 @@
+﻿using Game.Simulation;
+using System.Collections.Generic;
+using TMPro;
+
+namespace Game.GUI.Adventures
+{
+	public class AdventureTreasureUIComponent : AdventureUIComponent<AdventureTreasureComponent>
+	{
+		public TMP_Text treasureListText;
+		public TMP_Text descriptionText;
+
+		protected override List<TMP_Text> AssociatedTexts => new List<TMP_Text>() { treasureListText, descriptionText };
+
+		public override void BuildUIComponents(AdventureTreasureComponent component)
+		{
+			treasureListText.text = string.Empty;
+			descriptionText.text = component.description;
+
+			foreach (var treasure in component.treasures)
+			{
+				var id = "{" + treasure.treasureID + "}";
+				var entry = string.Format("<indent=1em>\u2022 {0} {1}. </indent> \n", treasure.amount, id);
+				treasureListText.text += entry;
+			}
+
+			var coins = string.Empty;
+			foreach(var value in component.currency)
+			{
+				coins += string.Format("{0} {1}, ", value.amount, value.coinType.ToString());
+			}
+
+			if(coins != string.Empty)
+			{
+				var entry = string.Format("<indent=1em>\u2022 {0}. </indent>", coins);
+				treasureListText.text += entry;
+			}
+		}
+	}
+}
