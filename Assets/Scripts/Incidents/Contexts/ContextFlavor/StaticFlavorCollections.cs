59788de3fffa3f6d5c826322cee23bf6c244ff24
Jackson Katzman <jackson@jacksonkatzman.com>
2023-10-06 14:15:23 -0500

    Add static collections for storing flavor conversions

---
+++ Assets/Scripts/Incidents/Contexts/ContextFlavor/StaticFlavorCollections.cs
@@ -0,0 +1,23 @@
+﻿using Game.Enums;
+using System.Collections.Generic;
+
+namespace Game.Incidents
+{
+	public static class StaticFlavorCollections
+	{
+		public static Dictionary<Gender, string> objectGenderPronouns = new Dictionary<Gender, string>()
+		{
+			{Gender.MALE, "him"}, {Gender.FEMALE, "her"}, {Gender.ANY, "them"}
+		};
+
+		public static Dictionary<Gender, string> subjectGenderPronouns = new Dictionary<Gender, string>()
+		{
+			{Gender.MALE, "he"}, {Gender.FEMALE, "she"}, {Gender.ANY, "they"}
+		};
+
+		public static Dictionary<Gender, string> possessiveGenderPronouns = new Dictionary<Gender, string>()
+		{
+			{Gender.MALE, "his"}, {Gender.FEMALE, "her"}, {Gender.ANY, "their"}
+		};
+	}
+}
