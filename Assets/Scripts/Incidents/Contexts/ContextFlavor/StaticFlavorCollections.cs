b41e4bf2ad1d7332f34541a8104b7fcd4bd0ec1b
Jackson Katzman <jackson@jacksonkatzman.com>
2023-10-07 10:08:43 -0500

    Move regex based flavor handling to StaticFlavorCollections

--- Assets/Scripts/Incidents/Contexts/ContextFlavor/StaticFlavorCollections.cs
+++ Assets/Scripts/Incidents/Contexts/ContextFlavor/StaticFlavorCollections.cs
@@ -1,10 +1,128 @@
 ﻿using Game.Enums;
+using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Game.Incidents
 {
 	public static class StaticFlavorCollections
 	{
+		public static string OBJECT_PRONOUN_STRING = "OBJP";
+		public static string SUBJECT_PRONOUN_STRING = "SUBP";
+		public static string POSSESSIVE_PRONOUN_STRING = "POSP";
+
+		public static Dictionary<string, Func<string, Dictionary<string, IIncidentContext>, string>> flavorFunctions = new Dictionary<string, Func<string, Dictionary<string, IIncidentContext>, string>>()
+		{
+			{OBJECT_PRONOUN_STRING, HandleObjectGenderPronouns}, {SUBJECT_PRONOUN_STRING, HandleSubjectGenderPronouns}, {POSSESSIVE_PRONOUN_STRING, HandlePossessiveGenderPronouns}
+		};
+
+		public static string InjectFlavor(string input, Dictionary<string, IIncidentContext> contexts)
+		{
+			var textLine = string.Copy(input);
+			var matches = Regex.Matches(textLine, @"\{(\w+):");
+
+			foreach(Match match in matches)
+			{
+				var tag = match.Groups[1].Value;
+				if(flavorFunctions.TryGetValue(tag, out var function))
+				{
+					textLine = function(textLine, contexts);
+				}
+			}
+
+			return textLine;
+		}
+
+		public static string HandleObjectGenderPronouns(string input, Dictionary<string, IIncidentContext> contexts)
+		{
+			var textLine = string.Copy(input);
+			var matches = Regex.Matches(textLine, @"\{" + OBJECT_PRONOUN_STRING + @":(\d+)\}");
+
+			foreach (Match match in matches)
+			{
+				var matchString = match.Value;
+				var matchId = match.Groups[1].Value;
+				var keyString = "{" + matchId + "}";
+				var linkedContext = contexts[keyString];
+
+				var pronounString = "OBJECT-PRONOUN";
+				if (linkedContext.GetType() == typeof(Character))
+				{
+					var character = linkedContext as Character;
+					pronounString = StaticFlavorCollections.objectGenderPronouns[character.Gender];
+				}
+				else
+				{
+					pronounString = "it";
+				}
+
+				var linkString = string.Format("<link=\"{0}\">{1}</link>", linkedContext.ID, pronounString);
+				textLine = textLine.Replace(matchString, linkString);
+			}
+
+			return textLine;
+		}
+
+		public static string HandleSubjectGenderPronouns(string input, Dictionary<string, IIncidentContext> contexts)
+		{
+			var textLine = string.Copy(input);
+			var matches = Regex.Matches(textLine, @"\{" + SUBJECT_PRONOUN_STRING + @":(\d+)\}");
+
+			foreach (Match match in matches)
+			{
+				var matchString = match.Value;
+				var matchId = match.Groups[1].Value;
+				var keyString = "{" + matchId + "}";
+				var linkedContext = contexts[keyString];
+
+				var pronounString = "SUBJECT-PRONOUN";
+				if (linkedContext.GetType() == typeof(Character))
+				{
+					var character = linkedContext as Character;
+					pronounString = StaticFlavorCollections.subjectGenderPronouns[character.Gender];
+				}
+				else
+				{
+					pronounString = "it";
+				}
+
+				var linkString = string.Format("<link=\"{0}\">{1}</link>", linkedContext.ID, pronounString);
+				textLine = textLine.Replace(matchString, linkString);
+			}
+
+			return textLine;
+		}
+
+		public static string HandlePossessiveGenderPronouns(string input, Dictionary<string, IIncidentContext> contexts)
+		{
+			var textLine = string.Copy(input);
+			var matches = Regex.Matches(textLine, @"\{" + POSSESSIVE_PRONOUN_STRING + @":(\d+)\}");
+
+			foreach (Match match in matches)
+			{
+				var matchString = match.Value;
+				var matchId = match.Groups[1].Value;
+				var keyString = "{" + matchId + "}";
+				var linkedContext = contexts[keyString];
+
+				var pronounString = "POS-PRONOUN";
+				if (linkedContext.GetType() == typeof(Character))
+				{
+					var character = linkedContext as Character;
+					pronounString = StaticFlavorCollections.possessiveGenderPronouns[character.Gender];
+				}
+				else
+				{
+					pronounString = "its";
+				}
+
+				var linkString = string.Format("<link=\"{0}\">{1}</link>", linkedContext.ID, pronounString);
+				textLine = textLine.Replace(matchString, linkString);
+			}
+
+			return textLine;
+		}
+
 		public static Dictionary<Gender, string> objectGenderPronouns = new Dictionary<Gender, string>()
 		{
 			{Gender.MALE, "him"}, {Gender.FEMALE, "her"}, {Gender.ANY, "them"}
