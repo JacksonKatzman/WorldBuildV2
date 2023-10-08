4f1ba6ad9eb02b61c15bc3c01e5e19129afb6d8d
Jackson Katzman <jackson@jacksonkatzman.com>
2023-10-08 16:19:52 -0500

    SerializedObjectCollections

---
+++ Assets/Scripts/Incidents/Contexts/ContextFlavor/SerializedObjectCollectionContainer.cs
@@ -0,0 +1,13 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Incidents
+{
+	[CreateAssetMenu(fileName = nameof(SerializedObjectCollectionContainer), menuName = "ScriptableObjects/Data/" + nameof(SerializedObjectCollectionContainer), order = 1)]
+	public class SerializedObjectCollectionContainer : SerializedScriptableObject
+	{
+		public Dictionary<Type, SerializedObjectCollection> collections;
+	}
+}
---
+++ Assets/Scripts/Incidents/Contexts/ContextFlavor/SerializedObjectCollectionService.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Game.Incidents
+{
+	public class SerializedObjectCollectionService
+	{
+		public SerializedObjectCollectionContainer container;
+
+		private static SerializedObjectCollectionService instance;
+		public static SerializedObjectCollectionService Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new SerializedObjectCollectionService();
+				}
+				return instance;
+			}
+		}
+
+		private SerializedObjectCollectionService()
+		{
+			container = Resources.Load<SerializedObjectCollectionContainer>("ScriptableObjects/Data/ObjectData");
+		}
+	}
+}
