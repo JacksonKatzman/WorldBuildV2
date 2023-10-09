8957c24cc461cdd37ed62086e88131537893b8f3
Jackson Katzman <jackson@jacksonkatzman.com>
2023-10-09 10:48:54 -0500

    Forgot to push the GeneologicalData class

---
+++ Assets/Scripts/Incidents/Contexts/GeneologicalData.cs
@@ -0,0 +1,20 @@
+﻿namespace Game.Incidents
+{
+	public class GeneologicalData
+	{
+		public int verticallyRemoved;
+		public int siblingallyRemoved;
+		public int spousallyRemoved;
+		public int finalValue;
+		public int iterations;
+
+		public GeneologicalData(int verticallyRemoved, int siblingallyRemoved, int spousallyRemoved, int finalValue, int iterations)
+		{
+			this.verticallyRemoved = verticallyRemoved;
+			this.spousallyRemoved = spousallyRemoved;
+			this.siblingallyRemoved = siblingallyRemoved;
+			this.finalValue = finalValue;
+			this.iterations = iterations;
+		}
+	}
+}
