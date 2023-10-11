dd33bddebbdc6541ae060f4035b6a78a28b183e8
Jackson Katzman <jackson@jacksonkatzman.com>
2023-10-10 22:23:28 -0500

    Refactor flavor types and add object collection

---
+++ Assets/Scripts/Incidents/Contexts/ContextFlavor/FlavorTemplate_Char.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Incidents
+{
+	[CreateAssetMenu(fileName = nameof(FlavorTemplate_Char), menuName = "ScriptableObjects/Flavors/" + nameof(FlavorTemplate_Char), order = 3)]
+	public class FlavorTemplate_Char : AbstractFlavorTemplate
+	{
+		public IndexedObject<Character> primaryCharacter = new IndexedObject<Character>(0);
+	}
+}
---
+++ Assets/Scripts/Incidents/Contexts/ContextFlavor/FlavorTemplate_CharFact.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Game.Incidents
+{
+	[CreateAssetMenu(fileName = nameof(FlavorTemplate_CharFact), menuName = "ScriptableObjects/Flavors/" + nameof(FlavorTemplate_CharFact), order = 4)]
+	public class FlavorTemplate_CharFact : AbstractFlavorTemplate
+	{
+		public IndexedObject<Character> primaryCharacter = new IndexedObject<Character>(0);
+		public IndexedObject<Faction> primaryFaction = new IndexedObject<Faction>(1);
+	}
+}
