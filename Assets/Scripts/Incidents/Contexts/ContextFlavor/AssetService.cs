0e525502cecd7a4a13ba90658de9e02fe60766d6
Jackson Katzman <jackson@jacksonkatzman.com>
2023-10-14 16:21:52 -0500

    Asset Service

---
+++ Assets/Scripts/Incidents/Contexts/ContextFlavor/AssetService.cs
@@ -0,0 +1,21 @@
+﻿using Sirenix.OdinInspector;
+
+namespace Game.Incidents
+{
+	public class AssetService : SerializedMonoBehaviour
+	{
+		public static AssetService Instance { get; private set; }
+		public TextCollection incidents;
+		public void Awake()
+		{
+			if (Instance != null && Instance != this)
+			{
+				Destroy(this);
+			}
+			else
+			{
+				Instance = this;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Incidents/TextCollection.cs
@@ -0,0 +1,42 @@
+﻿using Game.Utilities;
+using Newtonsoft.Json;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Game.Incidents
+{
+	[CreateAssetMenu(fileName = nameof(TextCollection), menuName = "ScriptableObjects/Data/" + nameof(TextCollection), order = 1)]
+	public class TextCollection : SerializedScriptableObject
+	{
+		public List<TextAsset> texts;
+
+		public string resourcePath;
+
+		[Button("Compile Objects")]
+		private void CompileObjects()
+		{
+			var files = Directory.GetFiles(Application.dataPath + $"/Resources/{resourcePath}/", "*.json");
+			texts = new List<TextAsset>();
+			foreach (string file in files)
+			{
+				var title = file.Replace(Application.dataPath + "/Resources/", "").Replace(".json", "");
+				var asset = Resources.Load<TextAsset>(title);
+				texts.Add(asset);
+			}
+			/*
+			var files = Directory.GetFiles(Application.dataPath + SaveUtilities.INCIDENT_DATA_PATH, "*.json");
+			foreach (string file in files)
+			{
+				var text = File.ReadAllText(file);
+				if (text.Length > 0)
+				{
+					var item = JsonConvert.DeserializeObject<Incident>(text, SaveUtilities.SERIALIZER_SETTINGS);
+					incidents.Add(item);
+				}
+			}
+			*/
+		}
+	}
+}
