b6dc7b7c9ae1eeea7193c441f9c021545e3a9bd2
Jackson Katzman <jackson@jacksonkatzman.com>
2023-11-01 16:01:06 -0500

    Clean up duplicate events

--- Assets/Scripts/Simulation/EventManagement/AffiliatedOrganizationChangedEvent.cs
+++ Assets/Scripts/Simulation/EventManagement/AffiliatedOrganizationChangedEvent.cs
@@ -3,15 +3,5 @@ using Game.Incidents;
 
 namespace Game.Simulation
 {
-	public class AffiliatedOrganizationChangedEvent : ISimulationEvent
-	{
-        public ISentient affiliate;
-        public IOrganizationPosition newPosition;
-
-		public AffiliatedOrganizationChangedEvent(ISentient affiliate, IOrganizationPosition newPosition)
-		{
-			this.affiliate = affiliate;
-			this.newPosition = newPosition;
-		}
-	}
+	
 }
