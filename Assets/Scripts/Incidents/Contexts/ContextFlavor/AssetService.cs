ee33b047f9b8c1f7f0155f44da5dea8f688f30fb
Jackson Katzman <jackson@jacksonkatzman.com>
2024-02-11 17:30:14 -0600

    Add basic billboard for debug etc

--- Assets/Scripts/Incidents/Contexts/ContextFlavor/AssetService.cs
+++ Assets/Scripts/Incidents/Contexts/ContextFlavor/AssetService.cs
@@ -15,6 +15,7 @@ namespace Game.Incidents
 		public Dictionary<CreatureType, NamingThemePreset> monsterPresets;
 		public Transform testMountain;
 		public Transform basePrefab;
+		public HexFeatureBillboard billboardPrefab;
 
 		[SerializeField]
 		private BiomeDataContainer biomeDataContainer;
---
+++ Assets/Scripts/Visuals/World/HexMap/Scripts/HexFeatureBillboard.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Terrain
+{
+    public class HexFeatureBillboard : MonoBehaviour
+    {
+        Camera mainCamera;
+        public TMP_Text tmpText;
+
+        private void Start()
+        {
+            mainCamera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            //transform.LookAt(mainCamera.transform);
+            //transform.Rotate(0, 180, 0);
+            transform.rotation = mainCamera.transform.rotation;
+        }
+    }
+}
